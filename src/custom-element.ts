b9cdce8cdec779ae3f5a1dfe63481e0757581bb1
Tyler Akins <fidian@rumkin.com>
2023-12-10 17:33:40 -0600

    Fixing shadowRoot removal again

--- src/custom-element.ts
+++ src/custom-element.ts
@@ -61,8 +61,12 @@ export class CustomElement extends HTMLElement {
         // Need to eliminate the hard reference to the element.
         // Everything else should be able to be garbage collected.
         metadataControllerElement.delete(controller);
-        this.shadowRoot!.innerHTML = '';
         this.innerHTML = '';
+
+        if (this.shadowRoot) {
+            this.shadowRoot.innerHTML = '';
+        }
+
         controller.onDestroy && controller.onDestroy();
     }
 }
