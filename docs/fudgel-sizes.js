003ed8d57ac44cf7d9e25c188d6bd75361154951
Tyler Akins <fidian@rumkin.com>
2023-12-20 11:58:46 -0600

    Updating documentation with component sizes

---
+++ docs/fudgel-sizes.js
@@ -0,0 +1,36 @@
+import { component, request } from './fudgel.min.js';
+
+component(
+    'fudgel-sizes',
+    {
+        style: /* css */ `
+            .flexCenter {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+        `,
+        template: /* html */ `
+            <div class="flexCenter">
+                <table>
+                    <tr><th></th><th>Minified</th><th>Gzipped</th></tr>
+                    <tr *for="row of this.data">
+                        <th>{{$scope.row?.name}}</th>
+                        <td>{{$scope.row?.minified}}</td>
+                        <td>{{$scope.row?.gzipped}}</td>
+                    </tr>
+                </table>
+            </div>
+        `,
+    },
+    class {
+        data = [];
+
+        onInit() {
+            request({ url: 'fudgel-sizes.json' }).then(result => {
+                console.log(result);
+                this.data = result;
+            });
+        }
+    }
+);
---
+++ src/only-di.ts
@@ -0,0 +1,1 @@
+export { di } from './fudgel';
