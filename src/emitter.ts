58cd547d89186f18f2801dc1d7653f54bf9fd307
Tyler Akins <fidian@rumkin.com>
2025-05-05 11:42:51 -0500

    Doc updates + allowing generic Emitter typings

--- src/emitter.ts
+++ src/emitter.ts
@@ -1,16 +1,16 @@
 export type EmitterCallback = (...args: any[]) => void;
 export type EmitterUnsubscribe = () => void;
 
-export class Emitter {
-    private _m = new Map<string, EmitterCallback[]>();
+export class Emitter<T = string> {
+    private _m = new Map<T, EmitterCallback[]>();
 
-    emit(name: string, ...data: any[]) {
+    emit(name: T, ...data: any[]) {
         for (const cb of [...this._c(name)]) {
             cb(...data);
         }
     }
 
-    off(name: string, callback: EmitterCallback) {
+    off(name: T, callback: EmitterCallback) {
         const list = this._c(name);
 
         for (let i = list.length - 1; i >= 0; i -= 1) {
@@ -20,10 +20,10 @@ export class Emitter {
         }
     }
 
-    on = (name: string, callback: EmitterCallback): (() => void) =>
+    on = (name: T, callback: EmitterCallback): (() => void) =>
         (this._c(name).push(callback) as unknown as true) &&
         (() => this.off(name, callback));
 
-    private _c = (name: string) =>
+    private _c = (name: T) =>
         this._m.get(name) || this._m.set(name, []).get(name)!;
 }
